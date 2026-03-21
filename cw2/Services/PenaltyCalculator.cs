c5f2f9c3e15cc0c6a2448f1fa5601c793e09450e
Piotr Sztenke <s33722@pjwstk.edu.pl>
2026-03-21 19:53:06 +0100

    dodalem serwisy do wypozyczen i kar za opoznienie

---
+++ cw2/Services/PenaltyCalculator.cs
@@ -0,0 +1,35 @@
+﻿using cw2.Models;
+
+namespace cw2.Services;
+
+public class PenaltyCalculator
+{
+    private const decimal DailyPenaltyRate = 10.0m;
+
+    public decimal CalculatePenalty(Rental rental)
+    {
+        DateTime endDate;
+        if (rental.ReturnDate.HasValue)
+        {
+            endDate = rental.ReturnDate.Value;
+        }
+        else
+        {
+            endDate = DateTime.Now;
+        }
+
+        if (endDate <= rental.DueDate)
+        {
+            return 0m;
+        }
+
+        var delayDays = (endDate - rental.DueDate).Days;
+
+        if (delayDays > 0)
+        {
+            return delayDays * DailyPenaltyRate;
+        }
+
+        return 0m;
+    }
+}
---
+++ cw2/Services/RentalService.cs
@@ -0,0 +1,80 @@
+﻿using cw2.Models;
+using cw2.Repositories;
+
+namespace cw2.Services;
+using System;
+using System.Collections.Generic;
+public class RentalService
+{
+    private readonly IEquipmentRepository _equipmentRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly List<Rental> _rentals = new List<Rental>();
+    
+    public RentalService(IEquipmentRepository equipmentRepo, IUserRepository userRepo)
+        {
+        _equipmentRepository = equipmentRepo;
+        _userRepository = userRepo;
+        }
+
+    public Rental RentEquipment(Guid equipmentId, Guid userId)
+    {
+        var equipment = _equipmentRepository.GetById(equipmentId);
+        var user = _userRepository.GetById(userId);
+
+        if (equipment == null || user == null)
+        {
+            throw new Exception("Nie znaleziono sprzetu lub osoby");
+        }
+        
+        bool isAlreadyRented = false;
+        foreach (var rental in _rentals)
+        {
+            if (rental.RentedEquipment.Id == equipmentId && rental.ReturnDate == null)
+            {
+                isAlreadyRented = true;
+                break;
+            }
+        }
+
+        if (isAlreadyRented)
+        {
+            throw new Exception("Sprzet zajety");
+        }
+
+        int activeRentalsCount = 0;
+        foreach (var rental in _rentals)
+        {
+            if (rental.RentedBy.Id == userId && rental.ReturnDate == null)
+            {
+                activeRentalsCount++;
+            }
+        }
+
+        if (activeRentalsCount >= user.MaxRentals)
+        {
+            throw new Exception($"Uzytkownik osiagnal juz swoj limit wypozyczen ({user.MaxRentals})");
+        }
+        var newRental = new Rental(equipment, user);
+        _rentals.Add(newRental);
+        return newRental;
+    }
+
+    public void ReturnEquipment(Guid equipmentId)
+    {
+        Rental activeRental = null;
+        foreach (var rental in _rentals)
+        {
+            if (rental.RentedEquipment.Id == equipmentId && rental.ReturnDate == null)
+            {
+                activeRental = rental;
+                break;
+            }
+        }
+
+        if (activeRental == null)
+        {
+            throw new Exception("Nie znaleziono aktywnego wypozyczenia dla tego sprzetu");
+        }
+        activeRental.MarkAsReturned();
+    }
+}
