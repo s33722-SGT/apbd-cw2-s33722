3417b6ce4adc4daa8360ef31b71d8c6d3ca8884c
Piotr Sztenke <s33722@pjwstk.edu.pl>
2026-03-21 18:39:01 +0100

    dodalem model wypozyczenia Rental

---
+++ cw2/Models/Rental.cs
@@ -0,0 +1,25 @@
+﻿namespace cw2.Models;
+using System;
+public class Rental
+{
+    public Guid Id { get; private set; }
+    public Equipment RentedEquipment { get; private set; }
+    public User RentedBy { get; private set; }
+    public DateTime RentalDate { get; private set; }
+    public DateTime DueDate { get; private set; }
+    public DateTime? ReturnDate { get; private set; }
+    
+    public Rental(Equipment equipment, User user, int rentDurationDays = 7)
+    {
+        Id = Guid.NewGuid();
+        RentedEquipment = equipment;
+        RentedBy = user;
+        RentalDate = DateTime.Now;
+        DueDate = DateTime.Now.AddDays(rentDurationDays);
+    }
+
+    public void MarkAsReturned()
+    {
+        ReturnDate = DateTime.Now;
+    }
+}
